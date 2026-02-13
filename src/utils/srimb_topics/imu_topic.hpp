4153317b42ba7b6ab44b918cf815fe08b0707361
r3ike <rpogliacomi@gmail.com>
2026-02-13 19:59:26 +0100

    - modifiche topic imu

--- src/utils/srimb_topics/imu_topic.hpp
+++ src/utils/srimb_topics/imu_topic.hpp
@@ -6,9 +6,15 @@
 
 
 struct ImuData {
-    Vector3f acc;
-    Vector3f gyro;
+    Vector3f raw_acc;
+    Vector3f raw_gyro;
+
+    //Imu dopo essere stato pre-filtrato => LPF, notch filter, temperature compensation, sculling e coning compensation
+    Vector3f filtered_acc;          
+    Vector3f filtered_gyro;
+
     int16_t temp;
+    //Da aggiungere scoring e stato sensore e capire se mettere quaternioni qua(quindi se fare la conversione nel driver o nel imuManager)
 };
 
 extern SRIMB_Topic<ImuData> topic_imu[IMU_INSTANCES];
