e44ebd8f6dba4b67c304af0e94413fddb018b667
r3ike <rpogliacomi@gmail.com>
2026-02-19 12:50:00 +0100

    - continuo creazione sistema di configurazioni - cmake driver da finire

--- cmake/module.cmake
+++ cmake/module.cmake
@@ -1,4 +1,4 @@
 function(add_module name)
     add_library(${name} STATIC)
-    target_include_directories(${name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/src)
+    target_include_directories(${name} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
 endfunction()
