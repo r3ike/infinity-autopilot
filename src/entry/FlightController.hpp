32fa4a252456b3973cfcca07557f859420564a70
r3ike <rpogliacomi@gmail.com>
2026-06-03 12:59:31 +0200

    feat(SRIMB): adattamenti al nuovo srimb

--- src/entry/FlightController.hpp
+++ src/entry/FlightController.hpp
@@ -4,7 +4,9 @@
 #include <zephyr/autoconf.h>
 #include <zephyr/sys/util.h>
 
+#include "SRIMB.hpp"
 #include "HAL.hpp"
+
 #ifdef CONFIG_LOGGER_ENABLED
 #include "LoggerTask.hpp"
 #endif
@@ -16,6 +18,8 @@
 class FlightController
 {
 private:
+    srimb::SRIMB<float> temp_topic;
+
     HAL _hal;
 
     ImuManagerTask _imu_manager_task;
