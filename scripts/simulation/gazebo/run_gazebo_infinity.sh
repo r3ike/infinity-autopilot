ecef53b975382f36fc8e46a9ea6360bbce1d90f9
r3ike <rpogliacomi@gmail.com>
2026-06-23 12:27:53 +0200

    feat(SITL): added gazebo start script

--- scripts/simulation/gazebo/run_gazebo_infinity.sh
+++ scripts/simulation/gazebo/run_gazebo_infinity.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
 # Questo script verrà eseguito da run_sitl_infinty.sh e si occuperà di avviare gazebo (capire che modello e world eseguire)
 # e inoltre avvierà il bridge per gazebo
+SCRIPT_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/../../.." && pwd)"
+
+# avvio gazebo
+
+source "$PROJECT_ROOT/.venv/bin/activate"
+
+python "$SCRIPT_DIR/infinity_gazebo_bridge.py"
